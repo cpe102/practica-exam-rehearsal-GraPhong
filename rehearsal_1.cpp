abd9df51a77450febb4039eedcb5d0b948e14db7
Phongsathon <i.am.phongsathon@gmail.com>
2020-01-10 16:31:37 +0700

    จำลองสอบ

--- rehearsal_1.cpp
+++ rehearsal_1.cpp
@@ -1,1 +1,20 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    string name;
+    float GPA;
+    cout << "What is your name = ";
+    cin >> name;
+
+    cout << "What is your GPA = ";
+    cin >> GPA;
+    if(GPA >= 3.50){
+        cout << name << " Inw Jrim Jrim!!!";
+    }else{
+        cout << "Try harder, "<< name <<"!!! ";
+    }
+    return 0;
+}
 
--- rehearsal_2.cpp
+++ rehearsal_2.cpp
@@ -1,1 +1,15 @@
+#include<iostream>
+using namespace std;
 
+int main(){
+    float x;
+    float sum = 0;
+    while(x != 0){
+        cout << "Enter x: ";
+        cin >> x;
+        if(x > 0)
+        sum = sum + x;
+        }
+    cout << "sum = "<<sum;    
+return 0;
+}
